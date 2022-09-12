b3234e76b634b3a82ed5c4b5a12e727ef33bcf84
doozles411 <cindy.phelps01@gmail.com>
2022-09-12 14:17:20 -0700

    Configure Babel

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,43 @@ class Header extends Component {
         };
     }
 
+    handleNameEdit = e => {
+        this.setState({ name: e.target.value });
+    }
+
+    handleTitleEdit = e => {
+        this.setState({ title: e.target.value });
+    }
+    
+    handleHeaderEdit = e => {
+        this.setState({ headerName: e.target.value });
+    }
+    
+    handleTextEdit = e => {
+        this.setState({ text: e.target.value });
+    }
+    
+    toggleEdit = e => {
+        this.setState(prevState => ({
+            edit: !prevState.edit
+        }));
+    }
+
     render() {
+        const { name, title, headerName, text, edit } = this.state;
+
         return (
             <div id="header">
-
+                <div className="user-name">
+                    <h1>{name ? name: 'Jane Doe'}</h1>
+                </div>
+                <div className="user-title">
+                    <h2>{title ? title: 'Entrepreneur'}</h2>
+                </div>
+                <div className="user-header">
+                    <h3>{headerName ? headerName: 'Profile'}</h3>
+                </div>
+                <div className="user-profile"></div>
             </div>
         )
     }
