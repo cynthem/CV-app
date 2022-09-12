9020a93ba62294b758ab3cbab6b819f2c427bfbc
doozles411 <cindy.phelps01@gmail.com>
2022-09-12 14:54:44 -0700

    Render placeholder div

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,1 +1,13 @@
 import React, { Component } from "react";
+
+class Education extends Component {
+    render() {
+        return (
+            <div id="education">
+                education
+            </div>
+        )
+    }
+}
+  
+export default Education;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,1 +1,13 @@
 import React, { Component } from "react";
+
+class Experience extends Component {
+    render() {
+        return (
+            <div id="experience">
+                experience
+            </div>
+        )
+    }
+}
+  
+export default Experience;
