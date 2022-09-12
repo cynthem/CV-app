72be9a1980e5964e85f258dacd25df451befc76c
doozles411 <cindy.phelps01@gmail.com>
2022-09-12 14:50:14 -0700

    Adjust Font Awesome settings

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,22 +34,45 @@ class Header extends Component {
 
         return (
             <div id="header">
-                <form className="user-name">
+                <form className="name-form">
                     <input
-                    className="user-name-input"
+                    className="name-input"
                     type="text"
-                    placeholder="Jane Doe"
+                    placeholder={name ? name: "Jane Doe"}
                     onChange={this.handleNameEdit}
                     />
                     <FontAwesomeIcon icon={solid('pen-to-square')} />
                 </form>
-                <div className="user-title">
-                    <h2>{title ? title: 'Entrepreneur'}</h2>
-                </div>
-                <div className="user-header">
-                    <h3>{headerName ? headerName: 'Profile'}</h3>
-                </div>
-                <div className="user-profile"></div>
+
+                <form className="title-form">
+                    <input
+                    className="title-input"
+                    type="text"
+                    placeholder={title ? title: "Entrepreneur"}
+                    onChange={this.handleTitleEdit}
+                    />
+                    <FontAwesomeIcon icon={solid('pen-to-square')} />
+                </form>
+
+                <form className="header-form">
+                    <input
+                    className="header-input"
+                    type="text"
+                    placeholder={headerName ? headerName: "Profile"}
+                    onChange={this.handleHeaderEdit}
+                    />
+                    <FontAwesomeIcon icon={solid('pen-to-square')} />
+                </form>
+
+                <form className="profile-form">
+                    <input
+                    className="profile-input"
+                    type="text"
+                    placeholder={text ? text: "This section is a great place to provide a brief statement or short summary of your career history and/or career intent in 2-5 sentences. Other titles for this section can include 'objective,' 'professional summary,' etc."}
+                    onChange={this.handleHeaderEdit}
+                    />
+                    <FontAwesomeIcon icon={solid('pen-to-square')} />
+                </form>
             </div>
         )
     }
