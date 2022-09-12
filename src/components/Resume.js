5d1b90189bfc4714525e3dec0e569b7ca89ec063
doozles411 <cindy.phelps01@gmail.com>
2022-09-12 13:39:09 -0700

    Import and display major components

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,1 +1,22 @@
 import React, { Component } from "react";
+import SidePanel from './SidePanel';
+import Header from './Header';
+import Experience from './Experience';
+import Education from './Education';
+
+class Resume extends Component {
+    render() {
+        return (
+            <div id="resume">
+                <SidePanel />
+                <main>
+                    <Header />
+                    <Experience />
+                    <Education />
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Resume;
