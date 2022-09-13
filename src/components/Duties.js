22cd582222d8ac41ac72499310e98a39542aac41
doozles411 <cindy.phelps01@gmail.com>
2022-09-13 10:29:13 -0700

    Render duties list

--- src/components/Duties.js
+++ src/components/Duties.js
@@ -8,11 +8,12 @@ class Duties extends Component {
         super();
         this.state = {
             duty: '',
-            duties: 
-            {
-                dutyId: uniqid(),
-                duty: ''
-            }
+            duties: [
+                {
+                    id: uniqid(),
+                    duty: ''
+                }
+            ]
         }
     }
 
@@ -22,47 +23,53 @@ class Duties extends Component {
 
     onClickDuty = () => {
         this.setState({
-            duty: '',
-            duties: this.state.experience.duties.concat({
-                dutyId: uniqid(),
+            duties: this.state.duties.concat({
+                id: uniqid(),
                 duty: this.state.duty
-            })
+            }),
+            duty: ''
         });
     }
 
     handleRemoveDuty = key => {
-        const removedDuty = this.state.duties.filter(item => item.dutyId !== key);
+        const removedDuty = this.state.duties.filter(item => item.id !== key);
         this.setState({ duty: removedDuty });
     }
 
-    <div className="experience-bottom">
-                                {duties?.map(item => {
-                                    return (
-                                        <div className="duty-form" key={item.dutyId}>
-                                            <FontAwesomeIcon 
-                                                icon={solid('circle')}
-                                                className="circle-icon"
-                                            />
-                                            <textarea
-                                                className="duty-input"
-                                                value={duty ? duty : "Add descriptions of your job responsibilities, achievements, etc., here. It's recommended to include 2-4 bullet points of 1-2 sentences each."}
-                                                onChange={this.handleDutyEdit}
-                                            >
-                                            </textarea>
-                                            <FontAwesomeIcon 
-                                                icon={solid('trash-can')}
-                                                className="trash-icon"
-                                                onClick={this.handleRemoveDuty(item.dutyId)}
-                                            />
-                                        </div>
-                                    )
-                                })}
-                                <FontAwesomeIcon 
-                                    icon={solid('plus')}
-                                    className="add-icon"
-                                    onClick={this.onClickDuty}
-                                />
-                            </div>
+    render() {
+        const { duties, duty } = this.state;
+
+        return (
+            <div className="experience-bottom">
+                {duties.map(item => {
+                    return (
+                        <div className="duty-item" key={item.id}>
+                            <FontAwesomeIcon 
+                                icon={solid('circle')}
+                                className="circle-icon"
+                            />
+                            <textarea
+                                className="duty-input"
+                                value={duty ? duty : "Add descriptions of your job responsibilities, achievements, etc., here. It's recommended to include 2-4 bullet points of 1-2 sentences each."}
+                                onChange={this.handleDutyEdit}
+                            >
+                            </textarea>
+                            <FontAwesomeIcon 
+                                icon={solid('trash-can')}
+                                className="trash-icon"
+                                onClick={this.handleRemoveDuty(item.id)}
+                            />
+                        </div>
+                    )
+                })}
+                <FontAwesomeIcon 
+                    icon={solid('plus')}
+                    className="add-icon"
+                    onClick={this.onClickDuty}
+                />
+            </div>
+        )
+    }
 }
 
 export default Duties;
