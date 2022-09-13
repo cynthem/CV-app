ff6cc5e2571840c46c7f5ce6c7eb75c6c43d0974
doozles411 <cindy.phelps01@gmail.com>
2022-09-12 17:37:08 -0700

    Remove profile heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ class Header extends Component {
         this.state = {
             name: '',
             title: '',
-            headerName: '',
             text: ''
         };
     }
@@ -21,57 +20,55 @@ class Header extends Component {
         this.setState({ title: e.target.value });
     }
     
-    handleHeaderEdit = e => {
-        this.setState({ headerName: e.target.value });
-    }
-    
     handleTextEdit = e => {
         this.setState({ text: e.target.value });
     }
 
     render() {
-        const { name, title, headerName, text } = this.state;
+        const { name, title, text } = this.state;
 
         return (
             <div id="header">
                 <form className="name-form">
+                    <FontAwesomeIcon 
+                        icon={solid('pen-to-square')}
+                        className="edit-icon"
+                    />
                     <input
                     className="name-input"
                     type="text"
+                    maxLength="22"
                     placeholder={name ? name: "Jane Doe"}
                     onChange={this.handleNameEdit}
                     />
-                    <FontAwesomeIcon icon={solid('pen-to-square')} />
                 </form>
 
                 <form className="title-form">
+                    <FontAwesomeIcon 
+                        icon={solid('pen-to-square')}
+                        className="edit-icon"
+                    />
                     <input
                     className="title-input"
                     type="text"
+                    maxLength="70"
                     placeholder={title ? title: "Entrepreneur"}
                     onChange={this.handleTitleEdit}
                     />
-                    <FontAwesomeIcon icon={solid('pen-to-square')} />
-                </form>
-
-                <form className="header-form">
-                    <input
-                    className="header-input"
-                    type="text"
-                    placeholder={headerName ? headerName: "Profile"}
-                    onChange={this.handleHeaderEdit}
-                    />
-                    <FontAwesomeIcon icon={solid('pen-to-square')} />
                 </form>
 
                 <form className="profile-form">
-                    <input
-                    className="profile-input"
-                    type="text"
-                    placeholder={text ? text: "This section is a great place to provide a brief statement or short summary of your career history and/or career intent in 2-5 sentences. Other titles for this section can include 'objective,' 'professional summary,' etc."}
-                    onChange={this.handleHeaderEdit}
+                    <FontAwesomeIcon 
+                        icon={solid('pen-to-square')}
+                        className="edit-icon"
                     />
-                    <FontAwesomeIcon icon={solid('pen-to-square')} />
+                    <textarea
+                        className="profile-input"
+                        maxLength="480"
+                        onChange={this.handleTextEdit}
+                    >
+                        {text ? text: "This section is a great place to provide a short summary of your career history or a brief statement of your career objective in 2-5 sentences. Other titles for this section can include 'objective,' 'professional summary,' etc."}
+                    </textarea>
                 </form>
             </div>
         )
