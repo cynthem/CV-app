7264c6db566192ec07d30709f44220397187a9da
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 11:40:20 -0700

    Render side section components

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import SideSectionA from "./SideSectionA";
+import SideSectionB from "./SideSectionB";
 
 class SidePanel extends Component {
     render() {
         return (
             <div id="side-panel">
                 <Contact />
+                <SideSectionA />
+                <SideSectionB />
             </div>
         )
     }
