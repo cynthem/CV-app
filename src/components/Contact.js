4a1eabfc14a18c73cc92236dcffe0a3b898561ef
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 12:39:08 -0700

    Add icons and input fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,6 +17,18 @@ class Contact extends Component {
         this.setState({ phone: e.target.value });
     }
 
+    handleEmailEdit = e => {
+        this.setState({ email: e.target.value });
+    }
+
+    handleAddressEdit = e => {
+        this.setState({ address: e.target.value });
+    }
+
+    handleWebsiteEdit = e => {
+        this.setState({ website: e.target.value });
+    }
+
     render() {
         const { phone, email, address, website } = this.state;
 
@@ -31,14 +43,49 @@ class Contact extends Component {
                             />
                             <input
                                 className="phone-input"
-                                type="phone"
-                                //placeholder={phone ? phone : "1"}
+                                type="tel"
+                                maxLength="14"
+                                placeholder={phone ? phone : "555-555-5555"}
                                 onChange={this.handlePhoneEdit}
                             />
                         </div>
-                        <div className="email"></div>
-                        <div className="address"></div>
-                        <div className="website"></div>
+                        <div className="email">
+                            <FontAwesomeIcon 
+                                icon={solid('envelope')}
+                                className="email-icon"
+                            />
+                            <input
+                                className="email-input"
+                                type="email"
+                                //maxLength="14"
+                                placeholder={email ? email : "email@gmail.com"}
+                                onChange={this.handleEmailEdit}
+                            />
+                        </div>
+                        <div className="address">
+                            <FontAwesomeIcon 
+                                icon={solid('location-dot')}
+                                className="address-icon"
+                            />
+                            <textarea
+                                className="address-input"
+                                //maxLength="14"
+                                value={address ? address : "123 Address St. City, ST 90210"}
+                                onChange={this.handleAddressEdit}
+                            ></textarea>
+                        </div>
+                        <div className="website">
+                            <FontAwesomeIcon 
+                                icon={solid('globe')}
+                                className="website-icon"
+                            />
+                            <textarea
+                                className="website-input"
+                                //maxLength="14"
+                                value={website ? website : "www.website.com linkedin.com/in/yourname "}
+                                onChange={this.handleWebsiteEdit}
+                            ></textarea>
+                        </div>
                     </div>
                 </div>
             </div>
