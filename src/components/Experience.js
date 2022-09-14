f9a437abfcef1e22c839a02fb67b90ca14ba5967
doozles411 <cindy.phelps01@gmail.com>
2022-09-13 20:00:50 -0700

    Pass in key prop from Resume

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -61,70 +61,70 @@ class Experience extends Component {
         } = this.state;
 
         return (
-                <div className="experience-container" key=>
-                    <div className="experience-top">
+            <div className="experience-container" key={this.props.expId}>
+                <div className="experience-top">
+                    <input
+                        className="company-input"
+                        type="text"
+                        maxLength="28"
+                        placeholder={company ? company : "Company/Organization"}
+                        onChange={this.handleCompanyEdit}
+                    />
+                    <div className="experience-location">
                         <input
-                            className="company-input"
+                            className="city-input"
                             type="text"
-                            maxLength="28"
-                            placeholder={company ? company : "Company/Organization"}
-                            onChange={this.handleCompanyEdit}
+                            maxLength="18"
+                            placeholder={city ? city : "City,"}
+                            onChange={this.handleCityEdit}
                         />
-                        <div className="experience-location">
-                            <input
-                                className="city-input"
-                                type="text"
-                                maxLength="18"
-                                placeholder={city ? city : "City,"}
-                                onChange={this.handleCityEdit}
-                            />
-                            <input
-                                className="state-input"
-                                type="text"
-                                maxLength="2"
-                                placeholder={location ? location : "ST"}
-                                onChange={this.handleLocationEdit}
-                            />
-                        </div>
-                    </div>
-                    <div className="experience-middle">
                         <input
-                            className="position-input"
+                            className="state-input"
                             type="text"
-                            maxLength="40"
-                            placeholder={position ? position : "Job Title"}
-                            onChange={this.handlePositionEdit}
-                        />   
-                        <input
-                            className="dates-input"
-                            type="text"
-                            maxLength="22"
-                            placeholder={dates ? dates : "Start date - End date"}
-                            onChange={this.handleDatesEdit}
+                            maxLength="2"
+                            placeholder={location ? location : "ST"}
+                            onChange={this.handleLocationEdit}
                         />
-                    </div>
-                    <div className="experience-bottom">
-                        {[...renderedDuties].map(item => {
-                            return (
-                                <div className="duty-render" key={item.id}>
-                                    <Duties key={item.id} dutyId={item.id} />
-                                    <FontAwesomeIcon 
-                                        icon={solid('trash-can')}
-                                        className="trash-icon"
-                                        onClick={() => this.removeDuty(item.id)}
-                                    />
-                                </div>
-                            )
-                        })}
-                        <div className="duties-edit">
-                            <FontAwesomeIcon 
-                                icon={solid('plus')}
-                                className="add-icon"
-                                onClick={() => this.renderDuty()}
-                            />
-                        </div>
                     </div>
                 </div>
+                <div className="experience-middle">
+                    <input
+                        className="position-input"
+                        type="text"
+                        maxLength="40"
+                        placeholder={position ? position : "Job Title"}
+                        onChange={this.handlePositionEdit}
+                    />   
+                    <input
+                        className="dates-input"
+                        type="text"
+                        maxLength="22"
+                        placeholder={dates ? dates : "Start date - End date"}
+                        onChange={this.handleDatesEdit}
+                    />
+                </div>
+                <div className="experience-bottom">
+                    {[...renderedDuties].map(item => {
+                        return (
+                            <div className="duty-render" key={item.id}>
+                                <Duties key={item.id} dutyId={item.id} />
+                                <FontAwesomeIcon 
+                                    icon={solid('trash-can')}
+                                    className="trash-icon"
+                                    onClick={() => this.removeDuty(item.id)}
+                                />
+                            </div>
+                        )
+                    })}
+                    <div className="duties-edit">
+                        <FontAwesomeIcon 
+                            icon={solid('plus')}
+                            className="add-icon"
+                            onClick={() => this.renderDuty()}
+                        />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
