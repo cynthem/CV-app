bcc83ea7351b6d0622f7fc8f208784a8479f473c
doozles411 <cindy.phelps01@gmail.com>
2022-09-13 20:36:29 -0700

    Create education methods

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,54 @@
 import React, { Component } from "react";
+import uniqid from 'uniqid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class Education extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            school: '',
+            degree: '',
+            city: '',
+            location: '',
+            date: '',
+            bullets: [
+                {
+                    id: uniqid()
+                }
+            ]
+        }
+    }
+
+    handleSchoolEdit = e => {
+        this.setState({ school: e.target.value });
+    }
+
+    handleDegreeEdit = e => {
+        this.setState({ degree: e.target.value });
+    }
+
+    handleCityEdit = e => {
+        this.setState({ city: e.target.value });
+    }
+
+    handleLocationEdit = e => {
+        this.setState({ location: e.target.value });
+    }
+
+    handleDateEdit = e => {
+        this.setState({ date: e.target.value });
+    }
+
+    renderBullet = () => {
+        this.setState({ bullets: [...this.state.bullets, { id: uniqid() }]});
+    }
+
+    removeBullet = key => {
+        const bulletPoints = this.state.bullets.filter(point => point.id !== key);
+        this.setState({ bullets: bulletPoints });
+    }
+
     render() {
         return (
             <div>hello</div>
