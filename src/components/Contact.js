035c9974de17b461f088c54cc87d09c119421e89
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 12:23:09 -0700

    Create contact section styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            phone: '',
+            email: '',
+            address: '',
+            website: ''
+        }
+    }
+
+    handlePhoneEdit = e => {
+        this.setState({ phone: e.target.value });
+    }
+
     render() {
+        const { phone, email, address, website } = this.state;
+
         return (
             <div className="contact-container">
                 <div className="contact">
                     <div className="contact-content">
-                        <p>contact</p>
+                        <div className="phone">
+                            <FontAwesomeIcon 
+                                icon={solid('phone')}
+                                className="phone-icon"
+                            />
+                            <input
+                                className="phone-input"
+                                type="phone"
+                                //placeholder={phone ? phone : "1"}
+                                onChange={this.handlePhoneEdit}
+                            />
+                        </div>
+                        <div className="email"></div>
+                        <div className="address"></div>
+                        <div className="website"></div>
                     </div>
                 </div>
             </div>
