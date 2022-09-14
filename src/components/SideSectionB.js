c62d16e1ed231783212d4d1accbc3054333bff2c
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 14:43:26 -0700

    Render bullet point items

--- src/components/SideSectionB.js
+++ src/components/SideSectionB.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class SideSectionB extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { item: '' };
+    }
+
+    handleItemEdit = e => {
+        this.setState({ item: e.target.value });
+    }
+
     render() {
+        const { item } = this.state;
+
         return (
-            <div className="sectionB-container">
-                <h3>Activities</h3>
+            <div className="sectionB-item" key={this.props.bKey}>
+                <FontAwesomeIcon 
+                    icon={solid('circle')}
+                    className="circle-icon"
+                />
+                <input
+                    className="sectionB-input"
+                    type="text"
+                    placeholder={item ? item : "Ideas: volunteering, awards"}
+                    onChange={this.handleItemEdit}
+                />
             </div>
         )
     }
