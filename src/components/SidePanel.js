5f093951b0fbe35716150735aa285ed90e9fa7f8
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 14:40:29 -0700

    Render bullet point items

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -2,13 +2,72 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 import SideSectionA from "./SideSectionA";
 import SideSectionB from "./SideSectionB";
+import uniqid from 'uniqid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class SidePanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sectionA: [
+                {
+                    id: uniqid()
+                }
+            ],
+            sectionB: [
+                {
+                    id: uniqid()
+                }
+            ]
+        }
+    }
+
+    renderSectionA = () => {
+        this.setState({ sectionA: [...this.state.sectionA, { id: uniqid() }]});
+    }
+
+    removeSectionA = key => {
+        const points = this.state.sectionA.filter(point => point.id !== key);
+        this.setState({ sectionA: points });
+    }
+
+    renderSectionB = () => {
+        this.setState({ sectionB: [...this.state.sectionB, { id: uniqid() }]});
+    }
+
+    removeSectionB = key => {
+        const points = this.state.sectionB.filter(point => point.id !== key);
+        this.setState({ sectionB: points });
+    }
+
     render() {
+        const { sectionA, sectionB } = this.state;
+
         return (
             <div id="side-panel">
                 <Contact />
-                <SideSectionA />
+                <div className="sectionA-container">
+                    <h3>Skills</h3>
+                    {[...sectionA].map(sec => {
+                        return (
+                            <div className="a-item">
+                                <FontAwesomeIcon 
+                                    icon={solid('trash-can')}
+                                    className="side-trash-icon"
+                                    onClick={() => this.removeSectionA(sec.id)}
+                                />
+                                <SideSectionA key={sec.id} aKey={sec.id}/>
+                            </div>
+                        )
+                    })}
+                    <FontAwesomeIcon 
+                        icon={solid('plus')}
+                        className="side-add-icon"
+                        onClick={this.renderExperience}
+                    />
+                </div>
+    
                 <SideSectionB />
             </div>
         )
--- src/components/SideSectionA.js
+++ src/components/SideSectionA.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class SideSectionA extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { point: '' };
+    }
+
+    handlePointEdit = e => {
+        this.setState({ point: e.target.value });
+    }
+
     render() {
+        const { point } = this.state;
+
         return (
-            <div className="sectionA-container">
-                <h3>Skills</h3>
+            <div className="sectionA-item" key={this.props.aKey}>
+                <FontAwesomeIcon 
+                    icon={solid('circle')}
+                    className="circle-icon"
+                />
+                <input
+                    className="sectionA-input"
+                    type="text"
+                    placeholder={point ? point : "Ideas: certifications, languages"}
+                    onChange={this.handlePointEdit}
+                />
             </div>
         )
     }
