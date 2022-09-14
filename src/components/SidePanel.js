8603a2b168d3c895f395bb2849bd5447045e3810
doozles411 <cindy.phelps01@gmail.com>
2022-09-14 11:37:07 -0700

    Initial commit

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class SidePanel extends Component {
     render() {
         return (
             <div id="side-panel">
-                side panel
+                <Contact />
             </div>
         )
     }
