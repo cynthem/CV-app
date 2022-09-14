e30fb6baaa7526446992e647bf1632634faa5017
doozles411 <cindy.phelps01@gmail.com>
2022-09-13 19:30:36 -0700

    Change default profile text

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ class Header extends Component {
                 <textarea
                     className="profile-input"
                     maxLength="500"
-                    value={text ? text : "This section is a great place to provide a short summary of your career history or a brief statement of your career objective in 2-5 sentences. Other titles for this section can include 'objective,' 'professional summary,' and more."}
+                    value={text ? text : "This section is a great place to provide a short summary of your career history or a brief statement of your career objective in 2-5 sentences. Many also use this space as an opportunity to explain a desired career transition and/or demonstrate past achievements."}
                     onChange={this.handleTextEdit}
                 >
                 </textarea>
